dccf392fcf7ed4a390ae99e01d760adf45711c80
k_krylowicz <kacper@krylowicz.pl>
2020-01-24 14:17:23 +0100

    :art: added color pallete to theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -7,5 +7,12 @@ export const theme = {
     regular: 400,
     bold: 700,
   },
-  primary: 'hsl(2, 76%, 48)'
+  color: {
+    primary: 'hsl(240, 8%, 9%)',
+    lightPrimary: 'hsl(216, 6%, 15%)',
+    secondary: 'hsl(255, 83%, 65%)',
+    tertiary: 'hsl(155, 61%, 44%)',
+    white: 'hsl(60, 100%, 100%)',
+    red: 'hsl(2, 76%, 48%)',
+  },
 };
