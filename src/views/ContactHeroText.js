79d63a75080c7b91fedcd2d37a8636cbeca8d73d
k_krylowicz <kacper@krylowicz.pl>
2020-01-27 22:21:30 +0100

    :sparkles: implemented email copiying to clipboad on clik

--- src/views/ContactHeroText.js
+++ src/views/ContactHeroText.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import Heading from 'components/Heading/Heading';
 import Paragraph from 'components/Paragraph/Paragraph';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Wrapper = styled.div`
   margin: 0 auto;
@@ -13,16 +13,58 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const ContactHeroText = () => (
-  <Wrapper>
-    <Heading big>contact@stacket.dev</Heading>
-    <Paragraph>
-      we know everybody hates these „mailto” links that open in the mail app.
-    </Paragraph>
-    <Paragraph down>
-      just click our email to copy it to your clipboard
-    </Paragraph>
-  </Wrapper>
-);
+const Heading = styled.span`
+  padding: 0;
+  margin: 0 0 12px 0;
+  font-size: 3.2rem;
+  font-family: ${({ theme }) => theme.font.jaf};
+  font-weight: ${({ theme }) => theme.font.regular};
+  color: ${({ theme, green }) =>
+    green ? theme.color.tertiary : theme.color.white};
+`;
+
+class ContactHeroText extends React.Component {
+  state = {
+    value: 'contact@stacket.dev',
+    copied: false,
+  };
+
+  onCopy = () => {
+    this.setState({ copied: true });
+  };
+
+  render() {
+    const { value, copied } = this.state;
+
+    return (
+      <Wrapper>
+        <CopyToClipboard onCopy={this.onCopy} text={value}>
+          {copied ? (
+            <Heading green>contact@stacket.dev</Heading>
+          ) : (
+            <Heading>contact@stacket.com</Heading>
+          )}
+        </CopyToClipboard>
+        {copied ? (
+          <>
+            <Paragraph>
+              Great! It&apos;s copied to your clipboard now!
+            </Paragraph>
+          </>
+        ) : (
+          <>
+            <Paragraph>
+              we know everybody hates these „mailto” links that open in the mail
+              app.
+            </Paragraph>
+            <Paragraph down>
+              just click our email to copy it to your clipboard
+            </Paragraph>
+          </>
+        )}
+      </Wrapper>
+    );
+  }
+}
 
 export default ContactHeroText;
